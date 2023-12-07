2c6e209fb24784a182d098b0993f0f4ea634fbcf
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2023-12-08 00:50:23 +0530

    chore: make route public

--- src/config.rs
+++ src/config.rs
@@ -16,15 +16,15 @@ pub enum Mode {
 
 /// Represents a route with its properties.
 #[derive(Serialize, Deserialize, JsonSchema)]
-struct Route {
+pub struct Route {
     /// The path of the route.
-    path: String,
+    pub path: String,
     /// Indicates if the route is dynamic.
-    dynamic: Option<bool>,
+    pub dynamic: Option<bool>,
     /// Indicates if the route is a catch-all.
-    catch_all: Option<bool>,
+    pub catch_all: Option<bool>,
     /// Children routes of the current route.
-    children: Option<Vec<Route>>,
+    pub children: Option<Vec<Route>>,
 }
 
 /// Represents a routing configuration.
